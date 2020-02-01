c74ee8cec65508cf52edfeac838e6829d00ad993
Yunbo Xie <yunboxie@Yunbos-MacBook-Pro.local>
2020-02-01 21:00:39 +0800

    use vuex

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -9,7 +9,7 @@
   	</div>
     <router-link to='/city'>
     	<div class="header-right">
-    	  {{this.$store.state.city}}
+    	  {{this.doubleCity}}
     	  <span class="iconfont arrow-icon">&#xe64a;</span>
       </div>
     </router-link>
@@ -17,8 +17,13 @@
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
 export default {
-  name: 'HomeHeader'
+  name: 'HomeHeader',
+  computed: {
+    ...mapState(['city']),
+    ...mapGetters(['doubleCity'])
+  }
 }	
 </script>
 
@@ -31,7 +36,7 @@ export default {
     color: #fff
     .header-left
       width: .64rem
-      float: left	
+      float: left
       .back-icon
         text-align: center
         font-size: .4rem
@@ -46,7 +51,8 @@ export default {
       border-radius: .1rem
       color: #ccc
     .header-right
-      width: 1.24rem
+      min-width: 1.04rem
+      padding: 0 .1rem
       float: right
       text-align: center
       color: #fff
